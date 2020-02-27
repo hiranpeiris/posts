f4b7cf790347485811f9513b7fbb1d10e41577cb
Hiran Peiris <hiranpeiris.sl@gmail.com>
2020-02-27 19:34:20 +0530

    init project

---
+++ App.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+const App = () => {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.msg}>Posts App!</Text>
+    </View>
+  );
+};
+
+export default App;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'white',
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  msg: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    color: 'green'
+  }
+});
